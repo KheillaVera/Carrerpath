372080d5b420ab8ff1b2804680ece988851a22ae
KheillaVera <igkvera47@gmail.com>
2026-03-11 14:56:35 +0200

    mark

--- api/get_results.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('../includes/db_connect.php');
-include('../includes/functions.php');
-// Get the category from the JS fetch request
-$interest = $_GET['category'] ?? 'Realistic';
-$data = getRecommendations($db, $interest); 
-header('Content-Type: application/json');
-echo json_encode($data); // This sends the data back to index.js
-?>
